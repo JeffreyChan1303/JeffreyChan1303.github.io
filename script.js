ef0711cbaf2b9d8d7f053eb1c11c1b69f1d47653
Jeffrey Chan <96323265+jfc2009@users.noreply.github.com>
2021-12-28 15:57:09 -0500

    intro animation almost finished

--- script.js
+++ script.js
@@ -3,20 +3,15 @@
 const creatorWebsiteLink = document.querySelector('.creator-website__link');
 const animations = document.querySelectorAll('.animation');
 
-for (let i = 0; i < animations.length; i++) {
-    let e = animations[i];
-    e.style.opacity = '0';
-}
+// animation for the intro
+
+
 
 for (let i = 0; i < animations.length; i++) {
         let e = animations[i];
         e.style.animation = '2s ease-out '+ (i*100) +'ms 1 intro-animation';
 }
 
-for (let i = 0; i < animations.length; i++) {
-    let e = animations[i];
-    e.style.opacity = '1';
-}
 
 // for (let i = 0; i < animations.length; i++) {
 //     let r = animations[i];
@@ -24,8 +19,22 @@ for (let i = 0; i < animations.length; i++) {
 // }
 
 
-
-
 creatorWebsiteLink.addEventListener('click', () => {
     document.body.classList.add('creator-website--opened')
+});
+
+// scroll animation 
+
+window.addEventListener('scroll', () => {
+    let effect = document.querySelector('.intro');
+    let content = document.querySelector('.education');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if (contentPosition < screenPosition) {
+        effect.style.animation = 'scroll-out 2s ease-in-out 0ms 1 forwards';
+    }
+    else {
+        effect.style.animation = 'scroll-in 2s ease-in-out 0ms 1 forwards';
+
+    }
 });
