dfbba958b8417932ca8039e900efceb11e2cf66f
Jeffrey Chan <96323265+jfc2009@users.noreply.github.com>
2022-01-01 02:06:16 -0500

    Working on the scroll animations of the portfolio website.

--- script.js
+++ script.js
@@ -25,16 +25,66 @@ creatorWebsiteLink.addEventListener('click', () => {
 
 // scroll animation 
 
+// window.addEventListener('scroll', () => {
+//     let effect = document.querySelector('.intro');
+//     let content = document.querySelector('.education');
+//     let contentPosition = content.getBoundingClientRect().top;
+//     let screenPosition = window.innerHeight;
+//     if (contentPosition < screenPosition) {
+//         effect.style.animation = 'scroll-out 1s ease-in-out 0ms 1 forwards';
+//     }
+//     else {
+//         effect.style.animation = 'scroll-in 1s ease-in-out 0ms 1 forwards';
+
+//     }
+// });
+
+// prallax for intro
+
+const vh = window.innerHeight;
+
 window.addEventListener('scroll', () => {
-    let effect = document.querySelector('.intro');
-    let content = document.querySelector('.education');
-    let contentPosition = content.getBoundingClientRect().top;
-    let screenPosition = window.innerHeight;
-    if (contentPosition < screenPosition) {
-        effect.style.animation = 'scroll-out 2s ease-in-out 0ms 1 forwards';
+    const target = document.querySelector('.intro');
+    var scrolled = window.pageYOffset;
+    var rate = scrolled / vh;
+
+    target.style.transform = "translate3d( 0vw, "+rate+"vh, "+-rate+"vw) scale3d( 1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew("+ 0 + 5 * rate +"deg, 0deg)";
+    target.style.opacity = 1 - rate + "";
+
+})
+
+//parallax for sections 
+
+window.addEventListener('scroll', () => {
+    const education = document.querySelector('.education');
+    const games = document.querySelector('.games');
+    const creatorWebsite = document.querySelector('.creator-website');
+
+    var scrolled = window.pageYOffset;
+
+    let contentPositionEducation = education.getBoundingClientRect().top;
+    let contentPositionGames = games.getBoundingClientRect().top;
+    let contentPositionCreatorWebsite = creatorWebsite.getBoundingClientRect().top;
+
+    console.log(vh);
+    var rate = 5 * (scrolled - contentPositionEducation) / vh;
+
+    if (contentPositionEducation + vh/2 > vh) {
+        var rate = (scrolled - contentPositionEducation + vh/2)/vh;
+        education.style.transfrom = "translate3d( 0vw, 0vh, 0vw) scale3d( 1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew( 0deg, 0deg)";
+        education.style.opacity = 3*rate + "";
+        
     }
-    else {
-        effect.style.animation = 'scroll-in 2s ease-in-out 0ms 1 forwards';
+    else { // finish the second part where u go below the content for the item to dissapear or fade out.
 
     }
+
+    if (contentPositionGames < vh) {
+
+    }
+
+    if (contentPositionCreatorWebsite < vh) {
+
+    }
+    console.log(contentPositionEducation);
 });
